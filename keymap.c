c0cbf5e57cd6b6e41efa1e5e79167a54fe3e26c6
Luca Critelli <lucacri@gmail.com>
2023-02-03 15:34:06 -0500

    Before mouse

--- keymap.c
+++ keymap.c
@@ -20,6 +20,7 @@
 #include "oled.c"
 #include "encoder.c"
 
+
 // Base layer is the number of layers CYCLE selects from.
 #define BASE_LAYERS 2
 
@@ -108,13 +109,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            |      |      |      |      |/       /          \      \ |      |      |      |      |
  *            `-----------------------------------'            '------''---------------------------'
  */
-
+// prettier-ignore
 [_QWERTY] = LAYOUT_via(
    QK_GRAVE_ESCAPE, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                         KC_6    , KC_7   , KC_8   , KC_9   , KC_0   , KC_GRV ,
    KC_ESC, KC_Q   , KC_UP   , KC_E   , KC_R   , KC_T   , KC_VOLU,       KC_PGUP, KC_Y    , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
    KC_TAB, KC_A   , KC_DOWN   , KC_D   , KC_F   , KC_G   , KC_MUTE,       KC_NO  , KC_H    , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_VOLD,       KC_PGDN, KC_N    , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-                  KC_LGUI,KC_LALT ,KC_LCTRL,MO(_LOWER), KC_ENT ,           KC_SPC  ,MO(_RAISE),KC_RCTRL, KC_RALT, KC_RGUI
+                  KC_LGUI,KC_LALT ,KC_LEFT_CTRL,MO(_LOWER), KC_ENT ,           KC_SPC  ,MO(_RAISE),KC_RIGHT_CTRL, KC_RALT, KC_RGUI
 ),
 /*
  * COLEMAK - Vanilla
@@ -137,7 +138,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC , KC_Q   , KC_W   , KC_F   , KC_P   , KC_G   , KC_VOLU,       KC_PGUP,  KC_J    , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_BSPC,
   KC_TAB , KC_A   , KC_R   , KC_S   , KC_T   , KC_D   , KC_MUTE,       KC_NO  ,  KC_H    , KC_N   , KC_E   , KC_I   , KC_O   , KC_QUOT,
   KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_VOLD,       KC_PGDN,  KC_N    , KC_M   , KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-                  KC_LGUI, KC_LALT,KC_LCTRL,MO(_LOWER), KC_ENT ,           KC_SPC ,MO(_RAISE),KC_RCTRL, KC_RALT, KC_RGUI
+                  KC_LGUI, KC_LALT,KC_LEFT_CTRL,MO(_LOWER), KC_ENT ,           KC_SPC ,MO(_RAISE),KC_RIGHT_CTRL, KC_RALT, KC_RGUI
 ),
 */
 /*
@@ -160,7 +161,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   , KC_VOLU,       KC_PGUP,  KC_J    , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_BSPC,
   KC_TAB , KC_A   , KC_R   , KC_S   , KC_T   , KC_G   , KC_MUTE,       KC_NO  ,  KC_M    , KC_N   , KC_E   , KC_I   , KC_O   , KC_QUOT,
   KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , KC_VOLD,       KC_PGDN,  KC_K    , KC_H   , KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-                  KC_LGUI, KC_LALT,KC_LCTRL,MO(_LOWER), KC_ENT ,           KC_SPC ,MO(_RAISE),KC_RCTRL, KC_RALT, KC_RGUI
+                  KC_LGUI, KC_LALT,KC_LEFT_CTRL,MO(_LOWER), KC_ENT ,           KC_SPC ,MO(_RAISE),KC_RIGHT_CTRL, KC_RALT, KC_RGUI
 ),
 
 /* LOWER
